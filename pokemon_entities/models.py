cdcbbdca9ab0743bc12d4c7e451d038a6040763c
unknown <egor1@mail.ru>
2022-01-17 02:03:32 +0300

    Add fields verbose name in Russian

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,15 +2,16 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200)
-    title_jp = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='pokemons')
+    title_ru = models.CharField('Название (рус)', max_length=200)
+    title_en = models.CharField('Название (анг)', max_length=200)
+    title_jp = models.CharField('Название (яп)', max_length=200)
+    description = models.TextField('Описание', blank=True)
+    image = models.ImageField('Изображение', upload_to='pokemons')
     previous_evolution = models.ForeignKey(
         'Pokemon',
         on_delete=models.SET_NULL,
         related_name='next_evolution',
+        verbose_name='Предыдущая эволюция',
         null=True,
         blank=True,
     )
@@ -24,16 +25,17 @@ class PokemonEntity(models.Model):
         Pokemon,
         on_delete=models.CASCADE,
         related_name='entity',
+        verbose_name='Покемон',
     )
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField(null=True)
-    disappeared_at = models.DateTimeField(null=True)
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strength = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+    appeared_at = models.DateTimeField('Появился', null=True)
+    disappeared_at = models.DateTimeField('Исчез', null=True)
+    level = models.IntegerField('Уровень')
+    health = models.IntegerField('Здоровье')
+    strength = models.IntegerField('Сила')
+    defence = models.IntegerField('Защита')
+    stamina = models.IntegerField('Выносливость')
 
     def __str__(self):
         return '{} at lat: {}, lon: {}'.format(
